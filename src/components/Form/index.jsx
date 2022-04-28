9bb5f4d297646d69d74db1e9043836f1a276edee
Fausto Azzaretti <99657715+fasuto14@users.noreply.github.com>
2022-04-28 15:48:05 -0300

    Add react-final-form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,42 +2,76 @@ import React from 'react';
 import './form.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { Form, Field } from 'react-final-form';
 
-const Form = () => {
+const required = value => value ? undefined : 'Required';
+
+const Formu = () => {
   return( 
   <div className="container">
        <div className="form">
-           <form action="">
-            <h1>Form for Register</h1>
-            <TextField 
-                id="outlined-basic" 
-                label="Email" 
-                variant="filled"
-                size="small"
-                margin="dense"
-                fullWidth 
-            />
-            <TextField 
-                id="outlined-basic"
-                type="password" 
-                label="Password" 
-                variant="filled"
-                size="small"
-                margin="dense"
-                fullWidth 
-            />
-            <TextField 
-                id="outlined-basic" 
-                label="Phone" 
-                variant="filled"
-                size="small"
-                margin="dense"
-                fullWidth 
-            />
-            <Button variant="contained">Register</Button>
-           </form>
+           <Form 
+                onSubmit={(formObj) => {
+                    console.log(formObj);
+                }}
+            >
+                {({handleSubmit,values,submitting})=> (
+                    <form onSubmit={handleSubmit}>
+                    <h1>Form for Register</h1>
+                        <Field name='email'>
+                        {({input}) =>(
+                            <TextField 
+                            id="outlined-basic" 
+                            label="Email" 
+                            variant="filled"
+                            size="small"
+                            margin="dense"
+                            fullWidth 
+                            {...input}
+                            validate={required}
+                        />
+                        )}
+                        </Field>
+                        <Field name="password">
+                        {({input})=>(
+                            <TextField 
+                            id="outlined-basic"
+                            type="password" 
+                            label="Password" 
+                            variant="filled"
+                            size="small"
+                            margin="dense"
+                            fullWidth
+                            {...input} 
+                            validate={required}
+                        />
+                        )}
+                        </Field>
+                        <Field name="phone">
+                        {({input}) =>(
+                            <TextField 
+                            id="outlined-basic" 
+                            label="Phone" 
+                            variant="filled"
+                            size="small"
+                            margin="dense"
+                            fullWidth
+                            {...input} 
+                        />
+                        )}
+                        </Field>
+                        <Button 
+                            variant="contained"
+                            type="submit"
+                        >
+                            Register
+                        </Button>
+                   </form>
+                )}
+               
+           </Form>
        </div>
   </div>)
 };
 
-export default Form;
+export default Formu;

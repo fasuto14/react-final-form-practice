faf81291e9e54244651db4cfc8215f818d119f24
Fausto Azzaretti <99657715+fasuto14@users.noreply.github.com>
2022-04-28 09:25:53 -0300

    Add TextField and Button in Form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import './form.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const Form = () => {
   return( 
   <div className="container">
        <div className="form">
-           <h1>Form for Register</h1>
-           
+           <form action="">
+            <h1>Form for Register</h1>
+            <TextField 
+                id="outlined-basic" 
+                label="Email" 
+                variant="filled"
+                size="small"
+                margin="dense"
+                fullWidth 
+            />
+            <TextField 
+                id="outlined-basic"
+                type="password" 
+                label="Password" 
+                variant="filled"
+                size="small"
+                margin="dense"
+                fullWidth 
+            />
+            <TextField 
+                id="outlined-basic" 
+                label="Phone" 
+                variant="filled"
+                size="small"
+                margin="dense"
+                fullWidth 
+            />
+            <Button variant="contained">Register</Button>
+           </form>
        </div>
   </div>)
 };
